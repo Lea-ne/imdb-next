1fd1a74502b04d60310e401ca89c2c8e345b4e33
leane lb <leaneleboulanger@gmail.com>
2024-07-26 14:46:54 +0100

    sauvegarde avant installation herIcon

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import MenuItem from "./MenuItem"
+
+export default function Header() {
+  return (
+    <div>
+      <div>
+        <MenuItem title="Home" adress="/" Icon=""/>
+      </div>
+      <div>
+      
+      </div>
+    </div>
+  )
+}
