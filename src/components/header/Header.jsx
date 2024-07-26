1c3b80fca982cbce5526da4774c826a845fb4980
leane lb <leaneleboulanger@gmail.com>
2024-07-26 16:22:19 +0100

    sauvegarder header ok

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,17 @@
-import MenuItem from "./MenuItem"
+// Header.jsx
+import { HomeIcon, SunIcon } from '@heroicons/react/24/solid'; // Assurez-vous que l'importation est correcte
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <div>
-      <div>
-        <MenuItem title="Home" adress="/" Icon=""/>
+      <div className="flex gap-4">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
       </div>
-      <div>
-      
+      <div className="flex gap-4">
+        <a href="/"><Image src="/logo.svg" width={64} height={64} className="hidden md:block" alt="Screenshots of the dashboard project showing desktop version"/></a>
       </div>
     </div>
-  )
+  );
 }
