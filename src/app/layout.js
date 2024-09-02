c6e59d7796399d1580b7dbcc62b8fe4f4864623f
leane lb <leaneleboulanger@gmail.com>
2024-09-02 19:51:49 +0100

    import avant test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from '@/components/header/Header'
+import Header from '@/components/header/Header';
 import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
