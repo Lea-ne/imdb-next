d82966c9c089dc289ea9b6b9346275f04954da85
leane lb <leaneleboulanger@gmail.com>
2024-09-02 17:31:07 +0100

    en train de faire le formalaire d'inscription, ajout de google authentification

---
+++ auth.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { db } from "./db";
+ 
+export const { 
+    handlers:{GET, POST},
+    signIn,
+    signOut,
+    auth
+
+    } = NextAuth({
+    adapter: PrismaAdapter(db),
+    session: {strategy: "jwt"},
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+        }),
+
+        Credentials({
+                
+        }),
+    ],
+})
